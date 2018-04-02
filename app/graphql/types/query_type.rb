7ea085c1e7dabec9c38f84e221f482225416f87b
raafa16 <raafa16@hotmail.com>
2018-04-02 16:18:50 +0600

    added root query to company or companies

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,18 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
+  field :companies, types[CompanyType] do
+    description "List all the companies"
     resolve ->(obj, args, ctx) {
-      "Hello World!"
+      Company.all
+    }
+  end
+
+  field :company, CompanyType do
+    argument :id, !types.ID
+    description 'Find a company'
+    resolve ->(obj, args, ctx) {
+      Company.find(args['id'])
     }
   end
 end
